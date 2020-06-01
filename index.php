f7e6893c87d0378eb3cc06aebb6dffc575423e60
Alvin Januar <aljan_r@outlook.co.id>
2020-06-01 19:30:35 +0700

    1st June addition

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Dialectify - Create familiar yet outrageous new dialects!</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='css/app.css'>
+    <script type="text/javascript" language='javascript' src='js/filler.js'></script>
 </head>
 <body>
     <div class="safezone">
@@ -18,10 +19,42 @@
                 </select>
                 <label for="dialect">Dialect: </label>
                 <select id="dialect" name="dialect">
-                    <option value="dutch">Dutch</option>
+                    <option value="dutch">--- Indo-European ---</option>
+                    <option value="dutch" selected="selected">Dutch</option>
+                    <option value="english">English</option>
+                    <option value="french">French</option>
+                    <option value="german">German</option>
+                    <option value="greek">Greek</option>
+                    <option value="italian">Italian</option>
+                    <option value="jutland">Jutland (Norwegian, Swedish, Danish)</option>
+                    <option value="latin">Latin</option>
+                    <option value="portuguese">Portuguese</option>
+                    <option value="russian">Russian</option>
+                    <option value="spanish">Spanish</option>
+                    <option value="sanskrit">--- Indian ---</option>
+                    <option value="hindi">Hindi</option>
+                    <option value="pali">Pali</option>
+                    <option value="sanskrit">Sanskrit</option>
+                    <option value="tamil">Tamil</option>
+                    <option value="javanese">--- Austronesian ---</option>
+                    <option value="balinese">Balinese</option>
+                    <option value="batak">Batak</option>
+                    <option value="betawi">Betawi</option>
+                    <option value="javanese">Javanese</option>
+                    <option value="minang">Minang</option>
+                    <option value="nias">Nias</option>
+                    <option value="sunda">Sunda</option>
+                    <option value="chinese">--- East Asian ---</option>
+                    <option value="chinese">Chinese</option>
+                    <option value="japanese">Japanese</option>
+                    <option value="korean">Korean</option>
+                    <option value="arabic">--- Middle East ---</option>
+                    <option value="arabic">Arabic</option>
+                    <option value="hebrew">Hebrew</option>
+                    <option value="persian">Persian</option>
                 </select><br>
                 <label for="input_text">Input pure text here:</label><br>
-                <textarea name="input_text" id="input_text" rows="10" required autofocus>Andi bermain ke pasar minggu bersama dengan mama. Ia menginginkan untuk membeli 50 buah epal merah dan satu boks mainan terkenal pada jaman papa dulu, sedangkan mamanya menginginkan 13 buah baju baru. Pamannya berpesan kepada mamanya Andi untuk mengingatkannya mengenai janjian pada pukul lima nanti.</textarea>
+                <textarea name="input_text" id="input_text" rows="10" required autofocus></textarea>
                 <br>
                 <input type="submit" value="Give dialects please, pretty sir!"><br>
                 <label>Sidenote: only Indonesian + Dutch dialect works now. This will be a proof of concept for an assignment. Those who aspire to contribute may do so <a href="https://github.com/sayastershire/dialectify">here</a>.</label>
@@ -29,7 +62,9 @@
 
             
         </div>
-        <div id="sidebar" style="display: none;">
+        <div id="sidebar">
+            <h2 class="title">Sample Text</h2>
+            <a onclick="filler1()" href="#">Sample Text 1</a><br>
             Cela est une barre du côte.
         </div>
     </div>
---
+++ js/filler.js
@@ -0,0 +1,7 @@
+// Variables and Examples
+var IndonesianSample1 = "Andi bermain ke pasar minggu bersama dengan mama. Ia menginginkan untuk membeli 50 buah epal merah dan satu boks mainan terkenal pada jaman papa dulu, sedangkan mamanya menginginkan 13 buah baju baru. Pamannya berpesan kepada mamanya Andi untuk mengingatkannya mengenai janjian pada pukul lima nanti."
+
+function filler1(){
+    document.getElementById("input_text").innerText = IndonesianSample1;
+    return 0;
+}
